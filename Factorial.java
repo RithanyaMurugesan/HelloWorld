4bffce6605aaa1634d4dac2b493ac6977ae58d15
RithanyaMurugesan <rithaninfo@gmail.com>
2016-07-27 16:44:00 +0530

    Update and rename Rev.java to Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		int fact=1;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n>=0)
+		{
+		for(int i=1;i<=n;i++)
+		{
+			fact=fact*i;
+			
+		}
+		System.out.println(fact);
+	}
+	}
+
+}
