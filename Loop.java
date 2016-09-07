6462358cc8e056d77469c9a82f6ae7bc79bdd510
RithanyaMurugesan <rithaninfo@gmail.com>
2016-09-07 10:00:18 +0530

    Create Loop.java

---
+++ Loop.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Loop  {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str="Hello";
+		for(int i=0;i<5;i++)
+		{
+		System.out.println(str);	
+		}
+		sc.close();
+
+	}
+
+}
